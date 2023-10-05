ac9d4a111af2da9416805ff908541b5d8a5044a3
Abdul Waheed <86144678+captainWaheed@users.noreply.github.com>
2023-10-05 13:36:30 +0500

    Time: 0 ms (100%), Space: 44 MB (24.26%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -1,12 +1,18 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int sum =0;
-        int m=mat.length;
-        for(int i=0;i<m;i++){
-            sum += mat[i][i] + mat[i][m-i-1];
-        }
-        if(m % 2 != 0) 
-            sum -= mat[m/2][m/2];
-        return sum;
-        }
+        int sum=0;
+            int start = 0;
+            int end = mat[0].length-1;
+            int i=0;
+            while(start<mat.length && end>=0){
+                sum+=mat[i][start]+mat[i][end];
+                if(start==end){
+                    sum-=mat[i][start];
+                }
+                i++;
+                start++;
+                end--;
+            }
+            return sum;
+    }
 }
